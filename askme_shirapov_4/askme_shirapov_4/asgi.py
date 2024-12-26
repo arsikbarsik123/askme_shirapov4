42a3b6ae7b00725694c457e909793f4f19dc5c91
Arsalan <batyavzdanii749@gmail.com>
2024-12-26 20:49:04 +0300

    dz4

---
+++ askme_shirapov_4/askme_shirapov_4/asgi.py
@@ -0,0 +1,8 @@
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askme_shirapov.settings')
+
+application = get_asgi_application()
